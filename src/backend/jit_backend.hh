0bf961685a410a6a0014c92966a48cdcfa7831ca
Netesy Emmanuel <netesy1@gmail.com>
2025-12-13 21:31:17 +0100

    Use the libgccjit cpp api

--- src/backend/jit_backend.hh
+++ src/backend/jit_backend.hh
@@ -2,7 +2,7 @@
 #define JIT_BACKEND_H
 
 #include "../frontend/ast.hh"
-#include <libgccjit.h>
+#include "libgccjit++.h"
 #include <memory>
 #include <string>
 #include <vector>
@@ -12,7 +12,7 @@
 class JitBackend {
 public:
     JitBackend();
-    ~JitBackend();
+    ~JitBackend() = default;
 
     void process(const std::vector<std::shared_ptr<AST::Program>>& programs);
     void compile(const char* output_filename);
@@ -38,52 +38,52 @@ private:
     void visit(const std::shared_ptr<AST::ModuleDeclaration>& stmt);
     void visit(const std::shared_ptr<AST::ImportStatement>& stmt);
 
-    gcc_jit_type* get_jit_type(const std::shared_ptr<AST::TypeAnnotation>& type);
+    gccjit::type get_jit_type(const std::shared_ptr<AST::TypeAnnotation>& type);
 
-    gcc_jit_rvalue* visit_expr(const std::shared_ptr<AST::Expression>& expr);
-    gcc_jit_rvalue* visit_expr(const std::shared_ptr<AST::BinaryExpr>& expr);
-    gcc_jit_rvalue* visit_expr(const std::shared_ptr<AST::UnaryExpr>& expr);
-    gcc_jit_rvalue* visit_expr(const std::shared_ptr<AST::LiteralExpr>& expr);
-    gcc_jit_rvalue* visit_expr(const std::shared_ptr<AST::VariableExpr>& expr);
-    gcc_jit_rvalue* visit_expr(const std::shared_ptr<AST::AssignExpr>& expr);
-    gcc_jit_rvalue* visit_expr(const std::shared_ptr<AST::CallExpr>& expr);
-    gcc_jit_rvalue* visit_expr(const std::shared_ptr<AST::LambdaExpr>& expr);
-    gcc_jit_rvalue* visit_expr(const std::shared_ptr<AST::GroupingExpr>& expr);
-    gcc_jit_rvalue* visit_expr(const std::shared_ptr<AST::MemberExpr>& expr);
+    gccjit::rvalue visit_expr(const std::shared_ptr<AST::Expression>& expr);
+    gccjit::rvalue visit_expr(const std::shared_ptr<AST::BinaryExpr>& expr);
+    gccjit::rvalue visit_expr(const std::shared_ptr<AST::UnaryExpr>& expr);
+    gccjit::rvalue visit_expr(const std::shared_ptr<AST::LiteralExpr>& expr);
+    gccjit::rvalue visit_expr(const std::shared_ptr<AST::VariableExpr>& expr);
+    gccjit::rvalue visit_expr(const std::shared_ptr<AST::AssignExpr>& expr);
+    gccjit::rvalue visit_expr(const std::shared_ptr<AST::CallExpr>& expr);
+    gccjit::rvalue visit_expr(const std::shared_ptr<AST::LambdaExpr>& expr);
+    gccjit::rvalue visit_expr(const std::shared_ptr<AST::GroupingExpr>& expr);
+    gccjit::rvalue visit_expr(const std::shared_ptr<AST::MemberExpr>& expr);
 
-    gcc_jit_context* m_context;
-    gcc_jit_function* m_main_func;
-    gcc_jit_function* m_current_func;
-    gcc_jit_block* m_current_block;
-    std::vector<std::unordered_map<std::string, gcc_jit_lvalue*>> m_scopes;
+    gccjit::context m_context;
+    gccjit::function m_main_func;
+    gccjit::function m_current_func;
+    gccjit::block m_current_block;
+    std::vector<std::unordered_map<std::string, gccjit::lvalue>> m_scopes;
 
     // Types
-    std::map<gcc_jit_type*, std::string> m_type_names;
-    std::map<gcc_jit_struct*, std::vector<std::string>> m_struct_field_names;
-    gcc_jit_type* m_void_type;
-    gcc_jit_type* m_int_type;
-    gcc_jit_type* m_double_type;
-    gcc_jit_type* m_bool_type;
-    gcc_jit_type* m_const_char_ptr_type;
-    gcc_jit_type* m_int8_type;
-    gcc_jit_type* m_int16_type;
-    gcc_jit_type* m_int32_type;
-    gcc_jit_type* m_int64_type;
-    gcc_jit_type* m_uint8_type;
-    gcc_jit_type* m_uint16_type;
-    gcc_jit_type* m_uint32_type;
-    gcc_jit_type* m_uint64_type;
-    gcc_jit_type* m_float_type;
-    gcc_jit_type* m_long_double_type;
+    std::map<gccjit::type, std::string> m_type_names;
+    std::map<gccjit::struct_, std::vector<std::string>> m_struct_field_names;
+    gccjit::type m_void_type;
+    gccjit::type m_int_type;
+    gccjit::type m_double_type;
+    gccjit::type m_bool_type;
+    gccjit::type m_const_char_ptr_type;
+    gccjit::type m_int8_type;
+    gccjit::type m_int16_type;
+    gccjit::type m_int32_type;
+    gccjit::type m_int64_type;
+    gccjit::type m_uint8_type;
+    gccjit::type m_uint16_type;
+    gccjit::type m_uint32_type;
+    gccjit::type m_uint64_type;
+    gccjit::type m_float_type;
+    gccjit::type m_long_double_type;
 
     // Functions
-    gcc_jit_function* m_printf_func;
-    gcc_jit_function* m_strcmp_func;
-    gcc_jit_function* m_malloc_func;
-    std::unordered_map<std::string, gcc_jit_function*> m_functions;
+    gccjit::function m_printf_func;
+    gccjit::function m_strcmp_func;
+    gccjit::function m_malloc_func;
+    std::unordered_map<std::string, gccjit::function> m_functions;
 
     // Loop handling
-    std::vector<std::pair<gcc_jit_block*, gcc_jit_block*>> m_loop_blocks;
+    std::vector<std::pair<gccjit::block, gccjit::block>> m_loop_blocks;
 
     // Memory management
     MemoryManager<> m_mem_manager;
