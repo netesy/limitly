0dd39f9cef1683596acc10860edd05eff69b43aa
Netesy Emmanuel <netesy1@gmail.com>
2025-12-13 15:55:04 +0100

    Implement module system and enhance JIT backend

--- src/backend/jit_backend.hh
+++ src/backend/jit_backend.hh
@@ -7,13 +7,14 @@
 #include <string>
 #include <vector>
 #include <unordered_map>
+#include "memory.hh"
 
 class JitBackend {
 public:
     JitBackend();
     ~JitBackend();
 
-    void process(const std::shared_ptr<AST::Program>& program);
+    void process(const std::vector<std::shared_ptr<AST::Program>>& programs);
     void compile(const char* output_filename);
 
 private:
@@ -30,6 +31,14 @@ private:
     void visit(const std::shared_ptr<AST::ContinueStatement>& stmt);
     void visit(const std::shared_ptr<AST::IterStatement>& stmt);
     void visit(const std::shared_ptr<AST::MatchStatement>& stmt);
+    void visit(const std::shared_ptr<AST::FunctionDeclaration>& stmt);
+    void visit(const std::shared_ptr<AST::ReturnStatement>& stmt);
+    void visit(const std::shared_ptr<AST::ClassDeclaration>& stmt);
+    void visit(const std::shared_ptr<AST::ParallelStatement>& stmt);
+    void visit(const std::shared_ptr<AST::ModuleDeclaration>& stmt);
+    void visit(const std::shared_ptr<AST::ImportStatement>& stmt);
+
+    gcc_jit_type* get_jit_type(const std::shared_ptr<AST::TypeAnnotation>& type);
 
     gcc_jit_rvalue* visit_expr(const std::shared_ptr<AST::Expression>& expr);
     gcc_jit_rvalue* visit_expr(const std::shared_ptr<AST::BinaryExpr>& expr);
@@ -37,21 +46,39 @@ private:
     gcc_jit_rvalue* visit_expr(const std::shared_ptr<AST::LiteralExpr>& expr);
     gcc_jit_rvalue* visit_expr(const std::shared_ptr<AST::VariableExpr>& expr);
     gcc_jit_rvalue* visit_expr(const std::shared_ptr<AST::AssignExpr>& expr);
+    gcc_jit_rvalue* visit_expr(const std::shared_ptr<AST::CallExpr>& expr);
+    gcc_jit_rvalue* visit_expr(const std::shared_ptr<AST::LambdaExpr>& expr);
+    gcc_jit_rvalue* visit_expr(const std::shared_ptr<AST::GroupingExpr>& expr);
 
     gcc_jit_context* m_context;
     gcc_jit_function* m_main_func;
+    gcc_jit_function* m_current_func;
     gcc_jit_block* m_current_block;
     std::vector<std::unordered_map<std::string, gcc_jit_lvalue*>> m_scopes;
 
     // Types
+    gcc_jit_type* m_void_type;
     gcc_jit_type* m_int_type;
+    gcc_jit_type* m_double_type;
+    gcc_jit_type* m_bool_type;
     gcc_jit_type* m_const_char_ptr_type;
 
     // Functions
     gcc_jit_function* m_printf_func;
+    gcc_jit_function* m_strcmp_func;
+    gcc_jit_function* m_malloc_func;
+    std::unordered_map<std::string, gcc_jit_function*> m_functions;
 
     // Loop handling
     std::vector<std::pair<gcc_jit_block*, gcc_jit_block*>> m_loop_blocks;
+
+    // Memory management
+    MemoryManager<> m_mem_manager;
+    MemoryManager<>::Region m_region;
+
+    // Module handling
+    std::string m_current_module_name;
+    std::string mangle(const std::string& name);
 };
 
 #endif // JIT_BACKEND_H
